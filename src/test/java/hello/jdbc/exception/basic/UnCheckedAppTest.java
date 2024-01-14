114b3c7f1c58a98288c03b33ad0c355efbe465ba
jaemoon <woans5970@naver.com>
2024-01-14 22:09:03 +0900

    section 5: Exception - stack trace

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -15,6 +17,16 @@ public class UnCheckedAppTest {
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
+    }
+
     static class Controller {
         Service service = new Service();
 
@@ -22,7 +34,6 @@ public class UnCheckedAppTest {
             service.logic();
         }
     }
-
 
     static class Service {
         Repository repository = new Repository();
@@ -65,6 +76,5 @@ public class UnCheckedAppTest {
             super(cause);
         }
     }
-
 
 }
